151c604607d095e0b93aef2bf8a5d9ca3ce8022b
eddy0 <awindy369@gmail.com>
2018-05-09 20:41:56 -0700

    react
    react

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
+const { log } = require('./utils.js')
+
 const express = require('express')
-const path = require('path')
+const http = require('http')
+const socket = require('socket.io')
 const bodyParser = require('body-parser')
 const session = require('cookie-session')
 const nunjucks = require('nunjucks')
+const path = require('path')
 
 const app = express()
 
+
 const configTemplate = () => {
-    const env = nunjucks.configure('templates',{
+    const env = nunjucks.configure('templates', {
         express: app,
         noCache: true,
         autoescape: true,
@@ -15,65 +20,137 @@ const configTemplate = () => {
     })
 
     env.addFilter('formattedTime', (date) => {
-        const { formattedTime } = require('./filter/filter.js')
-        const s = formattedTime(date)
-        return s
+        let { formattedTime } = require('./filter.js')
+        let f = formattedTime(date)
+        return f
     })
 
-    env.addFilter('replace', (str) => {
-        const { replace } = require('./filter/filter.js')
-        const s = replace(str)
-        return s
+    env.addFilter('calendarDate', (date) => {
+        let { calendarDate } = require('./filter.js')
+        let f = calendarDate(date)
+        return f
+    })
+
+    env.addFilter('localeDate', (date) => {
+        let { localeDate } = require('./filter.js')
+        let f = localeDate(date)
+        return f
     })
 }
 
-
-const routesRegister = () => {
-    // const api = require('/routes/api')
-    // app.use('/api/', api)
-
-    const index = require('./routes/index')
+const registerRouter = () => {
+    const index = require('./routes/index.js')
     app.use('/', index)
 
-    const topic = require('./routes/topic')
-    app.use('/topic', topic)
+    const profile = require('./routes/profile.js')
+    app.use('/setting', profile)
+
+    const user = require('./routes/user.js')
+    app.use('/user', user)
+
+    const apiSign = require('./routes/api/sign.js')
+    app.use('/api/', apiSign)
+
+    const apiTopic = require('./routes/apiTopic.js')
+    app.use('/api/topic/', apiTopic)
+
+    const apiComment = require('./routes/apiComment.js')
+    app.use('/api/comment/', apiComment)
+
+    const apiUser = require('./routes/apiUser.js')
+    app.use('/api/user/', apiUser)
 }
 
 const configApp = () => {
+
     app.use(bodyParser.urlencoded({
-        extended: true,
+        extended: true
     }))
 
     app.use(bodyParser.json())
 
     app.use(session({
         secret: 'key',
-    }))
+        }
+    ))
 
-    const asset = path.join(__dirname, 'static')
-    app.use('/static', express.static(asset))
-
-    app.use( (request, response, next) => {
-        response.locals.flash = request.session.flash
-        delete request.session.flash
+    app.use((req, res, next)=> {
+        res.locals.flash = req.session.flash
+        delete req.session.flash
         next()
     })
 
+    app.use('/static', express.static(path.join(__dirname, 'static')))
+
     configTemplate()
 
-    routesRegister()
+
+    registerRouter()
+}
+
+
+// const run = (port=3000, host) => {
+//     const server = app.listen(port, host, () => {
+//         const address = server.address()
+//         log('app listening', `http://localhost:${address.port}`)
+//     })
+// }
+
+
+const configIO = (server) => {
+    let io = require('socket.io').listen(server)
+
+    app.use( (req, res, next) => {
+        req.io = io
+        next()
+    })
+
+    io.on('connection', function(socket){
+        let clients = Object.keys(io.sockets.sockets)
+        console.log(clients.length, " clients are connected");
+        console.log(socket.id)
+        socket.on('disconnect', function(){
+            console.log(Object.keys(io.sockets.sockets) + " clients disconnected");
+            console.log(socket.id)
+            let socketId = socket.id
+            let Chat = require('./models/chat')
+            let u = Chat.findBy('socketId', socketId)
+            log('u', u)
+            if (u !== null) {
+                let id = u.uid
+                socket.broadcast.emit('delete', id)
+            }
+        })
+    })
+
+
+    const chat = require('./routes/chat.js')
+    app.use('/chat', chat)
+
+
+
+    //
+    // io.on('connection', function(socket){
+    //     socket.broadcast.emit('hi', '')
+    // })
+
 }
 
 const run = (port=3000, host) => {
     const server = app.listen(port, host, () => {
         const address = server.address()
-        console.log(`listening to http://${address.address}:${address.port}`)
+        log('app listening', `http://localhost:${address.port}`)
     })
+    return server
+
 }
 
-if (require.main === module) {
+
+
+if (require.main === module){
     configApp()
-    const port = 5000
-    const host = '0.0.0.0'
-    run(port, host)
+    let host = '0.0.0.0'
+    let port = 7000
+    let server = run(port, host )
+    configIO(server)
 }
